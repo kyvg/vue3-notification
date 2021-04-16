e819db143f6edcc8219c643ae0aab60fee67868a
Evgeny Kirpichyov <e.kirpichyov@ya.ru>
2021-04-16 17:38:01 +0300

    2.2.4

---
+++ dist/index.d.ts
@@ -0,0 +1,35 @@
+import { App } from 'vue';
+
+declare const _default: {
+    install: (app: App, args?: NotificationPluginOptions) => void;
+};
+export default _default;
+
+declare interface NotificationOptions_2 {
+    id?: number;
+    title?: string;
+    text?: string;
+    type?: string;
+    group?: string;
+    duration?: number;
+    speed?: number;
+    data?: unknown;
+    clean?: boolean;
+    clear?: boolean;
+    ignoreDuplicates?: boolean;
+}
+export { NotificationOptions_2 as NotificationOptions }
+
+export declare interface NotificationPluginOptions {
+    name?: string;
+    componentName?: string;
+    velocity?: any;
+}
+
+export declare const notify: {
+    (args: NotificationOptions_2 | string): void;
+    close(id: unknown): void;
+};
+
+export { }
+}
