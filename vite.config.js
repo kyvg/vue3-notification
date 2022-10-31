bd0b5983c36fc92e813563ec45eec74db6923b40
kyvg <e.kirpichyov@ya.ru>
2022-11-01 01:29:52 +0300

    Fix styles issue (#33)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       name: 'test',
