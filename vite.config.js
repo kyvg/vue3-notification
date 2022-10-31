295e98ec0771436fb15822abf1b35828738f4348
Evgeny Kirpichyov <47474371+kyvg@users.noreply.github.com>
2022-10-31 18:42:57 +0300

    Use Vite build (#32)
    * Use vite lib build
    * Drop Node 12

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,27 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  root: 'demo',
   plugins: [vue()],
   build: {
-    outDir: path.resolve(__dirname, './docs'),
+    lib: {
+      name: 'test',
+      entry:  path.resolve(__dirname, './src/index.ts'),
+      fileName: (type) => `index.${type}.js`,
+    },
     emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.runtime.esm-browser.js',
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name === 'style.css') {
+            return 'index.css';
+          }
+          return assetInfo.name;
+        },
+      },
     },
   },
-  base: './',
 });
