2e2a60b9dc799d30f8bd87f2e4ee168caffb2ca0
kyvg <e.kirpichyov@ya.ru>
2023-10-18 00:31:35 +0300

    fix config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { defaultTheme, defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Vue.js Notification',
+  base: "/vue3-notification/",
   theme: defaultTheme({
     repo: "https://github.com/kyvg/vue3-notification",
     editLink: false,
