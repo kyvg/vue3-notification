9f4f831b3e51cc9f5d1b997be8a834ac36d995f9
kyvg <e.kirpichyov@ya.ru>
2023-12-07 23:34:59 +0200

    Export component instead of type

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { Plugin } from 'vue';
 import { install } from './plugin';
 export { notify, useNotification } from './notify';
 export type { NotificationsOptions, NotificationsPluginOptions, NotificationItem } from './types';
-import type Notifications from '@/components/Notifications.vue';
-
-export type Notifications = typeof Notifications;
+export { default as Notifications } from '@/components/Notifications.vue';
 
 export default {
   install,
