01720776aee4910ec9875b44e128d3d1e9ff7195
kyvg <e.kirpichyov@ya.ru>
2024-08-24 14:08:44 +0300

    3.3.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,12 @@
-import { AllowedComponentProps } from 'vue';
-import { ComponentCustomProps } from 'vue';
 import { ComponentOptionsMixin } from 'vue';
 import { DefineComponent } from 'vue';
 import { ExtractPropTypes } from 'vue';
 import { HTMLAttributes } from 'vue';
-import type { Plugin as Plugin_2 } from 'vue';
+import { JSX } from 'vue/jsx-runtime';
+import { Plugin as Plugin_2 } from 'vue';
 import { PropType } from 'vue';
+import { PublicProps } from 'vue';
 import { SlotsType } from 'vue';
-import { VNodeProps } from 'vue';
 
 declare const _default: Plugin_2;
 export default _default;
@@ -17,199 +16,199 @@ export declare type NotificationItem = Pick<NotificationsOptions, 'id' | 'title'
     duplicates: number;
 };
 
-export declare const Notifications: DefineComponent<{
-    group: {
-        type: StringConstructor;
-        default: string;
-    };
-    /**
-     * Width of notification holder, can be `%`, `px` string or number.
-     * @example '100%', '200px', 200
-     * */
-    width: {
-        type: (StringConstructor | NumberConstructor)[];
-        default: number;
-    };
-    reverse: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    position: {
-        type: PropType<string | string[]>;
-        default: () => string[];
-    };
-    classes: {
-        type: PropType<string | string[]>;
-        default: string;
-    };
-    animationType: {
-        type: PropType<"css" | "velocity">;
-        default: string;
-        validator(value: unknown): boolean;
-    };
-    animation: {
-        type: PropType<Record<"enter" | "leave", unknown>>;
-        default(): {
-            enter: (el: Element) => {
-                height: number[];
-                opacity: number[];
-            };
-            leave: {
-                height: number;
-                opacity: number[];
-            };
-        };
-    };
-    animationName: {
-        type: StringConstructor;
-        default: string;
-    };
-    speed: {
-        type: NumberConstructor;
-        default: number;
-    };
-    /** Time (in ms) to keep the notification on screen (if **negative** - notification will stay **forever** or until clicked) */
-    duration: {
-        type: NumberConstructor;
-        default: number;
-    };
-    delay: {
-        type: NumberConstructor;
-        default: number;
-    };
-    max: {
-        type: NumberConstructor;
-        default: number;
-    };
-    ignoreDuplicates: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    closeOnClick: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    pauseOnHover: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    /** Use [v-html](https://vuejs.org/api/built-in-directives.html#v-html) to set `title` and `text` */
-    dangerouslySetInnerHtml: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
+export declare const Notifications: DefineComponent<    {
+group: {
+type: StringConstructor;
+default: string;
+};
+/**
+* Width of notification holder, can be `%`, `px` string or number.
+* @example '100%', '200px', 200
+* */
+width: {
+type: (StringConstructor | NumberConstructor)[];
+default: number;
+};
+reverse: {
+type: BooleanConstructor;
+default: boolean;
+};
+position: {
+type: PropType<string | string[]>;
+default: () => string[];
+};
+classes: {
+type: PropType<string | string[]>;
+default: string;
+};
+animationType: {
+type: PropType<"css" | "velocity">;
+default: string;
+validator(value: unknown): value is "css" | "velocity";
+};
+animation: {
+type: PropType<Record<"enter" | "leave", unknown>>;
+default(): {
+enter: (el: Element) => {
+height: number[];
+opacity: number[];
+};
+leave: {
+height: number;
+opacity: number[];
+};
+};
+};
+animationName: {
+type: StringConstructor;
+default: string;
+};
+speed: {
+type: NumberConstructor;
+default: number;
+};
+/** Time (in ms) to keep the notification on screen (if **negative** - notification will stay **forever** or until clicked) */
+duration: {
+type: NumberConstructor;
+default: number;
+};
+delay: {
+type: NumberConstructor;
+default: number;
+};
+max: {
+type: NumberConstructor;
+default: number;
+};
+ignoreDuplicates: {
+type: BooleanConstructor;
+default: boolean;
+};
+closeOnClick: {
+type: BooleanConstructor;
+default: boolean;
+};
+pauseOnHover: {
+type: BooleanConstructor;
+default: boolean;
+};
+/** Use [v-html](https://vuejs.org/api/built-in-directives.html#v-html) to set `title` and `text` */
+dangerouslySetInnerHtml: {
+type: BooleanConstructor;
+default: boolean;
+};
 }, () => JSX.Element, unknown, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, {
-    click: (item: NotificationItem) => true;
-    destroy: (item: NotificationItem) => true;
-    start: (item: NotificationItem) => true;
-}, string, VNodeProps & AllowedComponentProps & ComponentCustomProps, Readonly<ExtractPropTypes<{
-    group: {
-        type: StringConstructor;
-        default: string;
-    };
-    /**
-     * Width of notification holder, can be `%`, `px` string or number.
-     * @example '100%', '200px', 200
-     * */
-    width: {
-        type: (StringConstructor | NumberConstructor)[];
-        default: number;
-    };
-    reverse: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    position: {
-        type: PropType<string | string[]>;
-        default: () => string[];
-    };
-    classes: {
-        type: PropType<string | string[]>;
-        default: string;
-    };
-    animationType: {
-        type: PropType<"css" | "velocity">;
-        default: string;
-        validator(value: unknown): boolean;
-    };
-    animation: {
-        type: PropType<Record<"enter" | "leave", unknown>>;
-        default(): {
-            enter: (el: Element) => {
-                height: number[];
-                opacity: number[];
-            };
-            leave: {
-                height: number;
-                opacity: number[];
-            };
-        };
-    };
-    animationName: {
-        type: StringConstructor;
-        default: string;
-    };
-    speed: {
-        type: NumberConstructor;
-        default: number;
-    };
-    /** Time (in ms) to keep the notification on screen (if **negative** - notification will stay **forever** or until clicked) */
-    duration: {
-        type: NumberConstructor;
-        default: number;
-    };
-    delay: {
-        type: NumberConstructor;
-        default: number;
-    };
-    max: {
-        type: NumberConstructor;
-        default: number;
-    };
-    ignoreDuplicates: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    closeOnClick: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    pauseOnHover: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
-    /** Use [v-html](https://vuejs.org/api/built-in-directives.html#v-html) to set `title` and `text` */
-    dangerouslySetInnerHtml: {
-        type: BooleanConstructor;
-        default: boolean;
-    };
+click: (item: NotificationItem) => true;
+destroy: (item: NotificationItem) => true;
+start: (item: NotificationItem) => true;
+}, string, PublicProps, Readonly<ExtractPropTypes<    {
+group: {
+type: StringConstructor;
+default: string;
+};
+/**
+* Width of notification holder, can be `%`, `px` string or number.
+* @example '100%', '200px', 200
+* */
+width: {
+type: (StringConstructor | NumberConstructor)[];
+default: number;
+};
+reverse: {
+type: BooleanConstructor;
+default: boolean;
+};
+position: {
+type: PropType<string | string[]>;
+default: () => string[];
+};
+classes: {
+type: PropType<string | string[]>;
+default: string;
+};
+animationType: {
+type: PropType<"css" | "velocity">;
+default: string;
+validator(value: unknown): value is "css" | "velocity";
+};
+animation: {
+type: PropType<Record<"enter" | "leave", unknown>>;
+default(): {
+enter: (el: Element) => {
+height: number[];
+opacity: number[];
+};
+leave: {
+height: number;
+opacity: number[];
+};
+};
+};
+animationName: {
+type: StringConstructor;
+default: string;
+};
+speed: {
+type: NumberConstructor;
+default: number;
+};
+/** Time (in ms) to keep the notification on screen (if **negative** - notification will stay **forever** or until clicked) */
+duration: {
+type: NumberConstructor;
+default: number;
+};
+delay: {
+type: NumberConstructor;
+default: number;
+};
+max: {
+type: NumberConstructor;
+default: number;
+};
+ignoreDuplicates: {
+type: BooleanConstructor;
+default: boolean;
+};
+closeOnClick: {
+type: BooleanConstructor;
+default: boolean;
+};
+pauseOnHover: {
+type: BooleanConstructor;
+default: boolean;
+};
+/** Use [v-html](https://vuejs.org/api/built-in-directives.html#v-html) to set `title` and `text` */
+dangerouslySetInnerHtml: {
+type: BooleanConstructor;
+default: boolean;
+};
 }>> & {
-    onClick?: ((item: NotificationItem) => any) | undefined;
-    onDestroy?: ((item: NotificationItem) => any) | undefined;
-    onStart?: ((item: NotificationItem) => any) | undefined;
+onClick?: ((item: NotificationItem) => any) | undefined;
+onDestroy?: ((item: NotificationItem) => any) | undefined;
+onStart?: ((item: NotificationItem) => any) | undefined;
 }, {
-    speed: number;
-    group: string;
-    duration: number;
-    ignoreDuplicates: boolean;
-    closeOnClick: boolean;
-    reverse: boolean;
-    width: string | number;
-    position: string | string[];
-    classes: string | string[];
-    animationType: "css" | "velocity";
-    animation: Record<"enter" | "leave", unknown>;
-    animationName: string;
-    delay: number;
-    max: number;
-    pauseOnHover: boolean;
-    dangerouslySetInnerHtml: boolean;
+speed: number;
+group: string;
+duration: number;
+ignoreDuplicates: boolean;
+closeOnClick: boolean;
+reverse: boolean;
+width: string | number;
+position: string | string[];
+classes: string | string[];
+animationType: "css" | "velocity";
+animation: Record<"enter" | "leave", unknown>;
+animationName: string;
+delay: number;
+max: number;
+pauseOnHover: boolean;
+dangerouslySetInnerHtml: boolean;
 }, SlotsType<{
-    body?: ((props: {
-        class: HTMLAttributes['class'];
-        item: NotificationItem;
-        close: () => void;
-    }) => any) | undefined;
+body?: (props: {
+class: HTMLAttributes["class"];
+item: NotificationItem;
+close: () => void;
+}) => any;
 }>>;
 
 export declare interface NotificationsOptions {
@@ -248,7 +247,7 @@ export declare const useNotification: () => {
 };
 
 export { }
-declare module '@vue/runtime-core' {
+declare module 'vue' {
     export interface ComponentCustomProperties {
         $notify: typeof notify;
     }
